11917d6b5c8b1f261e37e2f198993f14025d9e28
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-05-08 08:46:03 +0530

    added getter and setters in js

--- js/AddressBookData.js
+++ js/AddressBookData.js
@@ -1,31 +1,33 @@
-/*UC 6 Addressbook contact JSON Object*/
 class AddressBookData {
-    get id() {
-        return this._id;
-    }
-    set id(id) {
-        this._id = id;
-    }
 
     get name() {
         return this._name;
     }
     set name(name) {
-        this._name = name;
-    }
-
-    get phoneNumber() {
-        return this.phoneNumber;
-    }
-    set phoneNumber(phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        let nameRegex = RegExp('^([A-Z]{1}[a-z]{2,}.)+$');
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw "Name is Incorrect";
     }
 
     get address() {
         return this._address;
     }
     set address(address) {
-        this._address = address;
+        let addressRegex = RegExp('^([A-Za-z0-9/.,-]{3,}.)+$')
+        if (addressRegex.test(address))
+            this._address = address;
+        else throw "Invalid Address"
+    }
+
+    get phoneNumber() {
+        return this._phoneNumber;
+    }
+    set phoneNumber(phoneNumber) {
+        let phoneRegex = RegExp("^[0-9]{10}$")
+        if (phoneRegex.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else throw "Invalid Phone Number"
     }
 
     get city() {
@@ -42,10 +44,23 @@ class AddressBookData {
         this._state = state;
     }
 
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+
     get zipcode() {
         return this._zipcode;
     }
     set zipcode(zipcode) {
         this._zipcode = zipcode;
+    }
+
+    // toString() method
+    toString() {
+        return "id : " + this.id + ", Name : " + this.name + ", Address : " + this.address + ", Phone : " + this.phoneNumber +
+            ", State : " + this.state + ", City : " + this.city + ", ZipCode : " + this.zipcode;
     }
 }
